fcb9704d1f053a815a76be2b63561b7766920942
yescoach <sbsii1020@gmail.com>
2022-07-06 11:18:29 +0900

    feat: 프로젝트 생성

---
+++ InfiniteCarousel/InfiniteCarousel/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  InfiniteCarousel
+//
+//  Created by Twave on 2022/07/06.
+//
+
+import UIKit
+
+class InfiniteCarousel: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
