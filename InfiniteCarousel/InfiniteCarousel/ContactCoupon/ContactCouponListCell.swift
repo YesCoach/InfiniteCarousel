e38da1dd66da8bb11025fbce6e51c1fea5fdc255
yescoach <sbsii1020@gmail.com>
2022-07-20 12:20:15 +0900

    refactor: 전화번호 정규식 적용

--- InfiniteCarousel/InfiniteCarousel/ContactCoupon/ContactCouponListCell.swift
+++ InfiniteCarousel/InfiniteCarousel/ContactCoupon/ContactCouponListCell.swift
@@ -65,7 +65,7 @@ class ContactCouponListCell: UITableViewCell {
                 thumbnailView.configure(with: newValue.givenName)
             }
             nameLabel.text = newValue.familyName + newValue.givenName
-            phoneNumberLabel.text = newValue.phoneNumbers[0].value.stringValue
+            phoneNumberLabel.text = newValue.phoneNumbers[0].value.stringValue.formattedPhoneNumber()
             if delegate.isAlreadySend(contact: newValue) {
                 sendButton.isHidden = true
             }
@@ -160,5 +160,40 @@ extension ContactCouponListCell: MFMessageComposeViewControllerDelegate {
             fatalError()
         }
         controller.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension String {
+    func formattedPhoneNumber() -> String {
+        let _str = self.replacingOccurrences(of: "-", with: "").replacingOccurrences(of: "/", with: "").replacingOccurrences(of: "+82", with: "0")
+        let arr = Array(_str)
+        if arr.count > 3 {
+            let prefix = String(format: "%@%@", String(arr[0]), String(arr[1]))
+            if prefix == "02" {
+                if let regex = try? NSRegularExpression(pattern: "([0-9]{2})([0-9]{3,4})([0-9]{4})", options: .caseInsensitive) {
+                    let modString = regex.stringByReplacingMatches(in: _str, options: [],
+                                                                   range: NSRange(_str.startIndex..., in: _str),
+                                                                   withTemplate: "$1-$2-$3")
+                    return modString
+                }
+                
+            } else if prefix == "15" || prefix == "16" || prefix == "18" {
+                if let regex = try? NSRegularExpression(pattern: "([0-9]{4})([0-9]{4})", options: .caseInsensitive) {
+                    let modString = regex.stringByReplacingMatches(in: _str, options: [],
+                                                                   range: NSRange(_str.startIndex..., in: _str),
+                                                                   withTemplate: "$1-$2")
+                    return modString
+                }
+            }
+            else {
+                if let regex = try? NSRegularExpression(pattern: "([0-9]{3})([0-9]{3,4})([0-9]{4})", options: .caseInsensitive) {
+                    let modString = regex.stringByReplacingMatches(in: _str, options: [],
+                                                                   range: NSRange(_str.startIndex..., in: _str),
+                                                                   withTemplate: "$1-$2-$3")
+                    return modString
+                }
+            }
+        }
+        return self
     }
 }
